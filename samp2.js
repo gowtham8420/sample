19a518174ae2c74a47ad9f398f8c796409e34e91
Gowtham <cheziangowtham@gmail.com>
2024-02-20 15:10:37 +0530

    newreb

--- samp2.js
+++ samp2.js
@@ -6,10 +6,6 @@ export const samp2 = () => {
       <div>samp2--gowtham-merge 1,2</div>
       <div>samp2--gowtham-merge 1,2 newly Added some</div>
     <div>samp2--gowtham-merge 1,2</div>
-      <div>samp2--gowtham-merge 1,2 newly Added some</div>
-    <div>samp2--gowtham-merge 1,2,HELLO ONE</div>
-    <div>samp2--gowtham-merge 1,2,HELLO ONE,three</div>
-    <div>samp2--gowtham-merge 1,2,HELLO ONE,TWO,three</div>
 
     </div>
 
