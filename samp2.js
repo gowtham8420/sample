8ec31848a34a0ecbadc0e5f668880f40643e4f08
gowtham8420 <139528004+gowtham8420@users.noreply.github.com>
2024-02-20 11:44:07 +0530

    Update samp2.js

--- samp2.js
+++ samp2.js
@@ -5,6 +5,10 @@ export const samp2 = () => {
     <div>samp2
       <div>samp2--gowtham-merge 1,2</div>
       <div>samp2--gowtham-merge 1,2 newly Added some</div>
+    <div>samp2--gowtham-merge 1,2</div>
+      <div>samp2--gowtham-merge 1,2 newly Added some</div>
+    <div>samp2--gowtham-merge 1,2</div>
+      <div>samp2--gowtham-merge 1,2 newly Added some</div>
     </div>
 
 
